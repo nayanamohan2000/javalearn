82f5a0e2df4f681a62f38183a595a63bbb10e9ec
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2020-09-20 15:49:50 +0530

    Create ForLoop1.java

---
+++ ForLoop1.java
@@ -0,0 +1,33 @@
+package learningjava;
+import java.util.Scanner;
+public class ForLoop1 {
+
+	public static void main(String[] args) {
+//		for(int i=0;i<=1;i++) {
+//		System.out.println("hello world");	
+//		} 
+		
+		
+//		System.out.println("all even number in the range one to hundred");
+//        for(int i=0;i<=100;i=i+2) {
+//        	System.out.println(i);
+//        }
+		
+		
+//		System.out.println("sum of n numbers");
+//		Scanner obj=new Scanner(System.in);
+//		System.out.println("enter n value");
+//		int n=obj.nextInt();
+//		int sum=0;
+//		int i;
+//		for(i=0;i<=n;i++)
+//		{
+//			sum=sum+i;
+//		}
+//		System.out.println("sum of n numbers is "+sum);
+		
+		
+		
+	}
+
+}
