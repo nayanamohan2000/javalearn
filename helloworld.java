ec36925822f979594afbebd9436070a9a453c5c1
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2020-09-20 15:48:11 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+package learningjava;
+
+public class Firstpgm {
+	
+
+	public static void main(String[] args) {
+		int i;
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+
+	}
+	}
