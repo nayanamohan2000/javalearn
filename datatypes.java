83de5d90596535679ac85680d972f1dfc61bef79
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2020-09-20 15:43:58 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,18 @@
+package learningjava;
+
+public class datatypes {
+
+    public static void main(String[] args)
+    {
+    	int age  =20;
+    	float rateOfInterst = 2.3f;
+    	double rate =3.1428571;
+    	System.out.println(rateOfInterst);
+    	System.out.println(age);
+    	System.out.println(rate);
+    	boolean cool = true;
+    	char mychar ='a';
+    	System.out.println(cool);
+    	System.out.println(mychar);
+}    
+}
