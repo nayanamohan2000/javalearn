2da4956d325cfe974aa5e58343cfc90b097088f6
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2020-09-20 15:55:36 +0530

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,27 @@
+package learningjava;
+
+public class operators {
+
+	public static void main(String[] args)
+	{
+//		int a=10;
+//		int b=30;
+//		int x=a*a+b*b+2*a*b;
+//		int y=(a+b)*(a+b);
+//		System.out.println(x);
+//		System.out.println(y);
+		
+		
+		int a=5;    //a=101 
+		int b=6;    //b=110
+		int c=a & b;//c=100=4 and 
+		int d=a | b;//d=111=7  or
+		int e=a<<1;//e=1010=10=2*5
+		int f=a>>1;//f=010=2=5/2
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+	}
+}
+		
