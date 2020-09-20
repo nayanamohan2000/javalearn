79bd8543767685a594b408e23ab0fb2fba9e88d3
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2020-09-20 15:59:28 +0530

    Create Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,24 @@
+package learningjava;
+import java.util.Scanner;
+public class Pattern5 {
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter the value of n");
+		int n=obj.nextInt();
+		int j;
+		for(int i=0;i<n;i++)
+		{
+			for(j=0;j<i;j++)
+			{
+				System.out.print("   ");
+			}
+			for(j=0;j<n-i;j++)
+			{
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+		}
+
+
+}
