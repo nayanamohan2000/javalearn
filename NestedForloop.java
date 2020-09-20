5dd97b7fff1b6cfc5b7b4b7528edb5541275fce8
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2020-09-20 15:53:51 +0530

    Create NestedForloop.java

---
+++ NestedForloop.java
@@ -0,0 +1,16 @@
+package learningjava;
+import java.util.Scanner;
+public class NestedForloop {
+	public static void main(String[] srgs)
+	{
+		Scanner obj=new Scanner(System.in);
+		
+	for(int j=1;j<=10;j++) {
+		for(int i=1;i<=20;i++)
+		{
+			System.out.print(j*i+" ");
+	}	
+		System.out.println();
+}
+}
+}
