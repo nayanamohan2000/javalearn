b9731441a0e802679a2df3e2affcce2620086c3a
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2020-09-20 16:10:15 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,11 @@
+package learningjava;
+
+public class TypeCasting {
+ public static void main(String[] args) {
+	  
+	 double y =3.14;
+	 int x = (int)y;
+	 System.out.println(x);
+	 System.out.println(y);
+ }
+}
