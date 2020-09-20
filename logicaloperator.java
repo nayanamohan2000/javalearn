cb209242dbd868268ce73e3c365275b742fcd277
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2020-09-20 15:52:54 +0530

    Create logicaloperator.java

---
+++ logicaloperator.java
@@ -0,0 +1,15 @@
+package learningjava;
+
+public class logicaloperator {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		int num=-1;
+//		if(num>=1 && num<=100) {
+//			System.out.println("num is in range");
+//		}
+		if(num>=-100||num<100) {
+			System.out.println("wow");
+		}
+	}
+
+}
